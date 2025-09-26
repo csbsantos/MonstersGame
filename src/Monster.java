5cac0fdbd148c34b378e07f63ad8f1e00276cc27
csbsantos <claudiasbsantos13@gmail.com>
2025-09-26 17:07:19 +0100

    Update Monster.java

--- src/Monster.java
+++ src/Monster.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+public abstract class Monster {
+private int life;
 
-public class Monster {
+    public Monster(int life) {
+        this.life = 100;
+    }
+
+    public int getLife() {
+        return life;
+    }
+
+    public void setLife(int life) {
+        this.life = life;
+    }
+
+    public String defend (int life){
+        int newLife= life-20;
+        return "Life updated into: " + newLife;
+    }
+
+    public abstract void attack(int life, Monster monster2);
 }
