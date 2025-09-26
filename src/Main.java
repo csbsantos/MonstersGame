27447151becc88e1195e3b9bec1c571ceb5f8b88
csbsantos <claudiasbsantos13@gmail.com>
2025-09-26 17:06:56 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         PlayerMonster player;
@@ -12,6 +10,8 @@ public class Main {
         game1.determineWinner(monstersP1, monstersP2);
     }
 }
+
+
 
 
 
