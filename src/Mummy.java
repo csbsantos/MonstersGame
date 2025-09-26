90f27ab575839eb6cc4a44459237c0458724ebcf
csbsantos <claudiasbsantos13@gmail.com>
2025-09-26 17:07:39 +0100

    Update Mummy.java

--- src/Mummy.java
+++ src/Mummy.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
+public class Mummy extends Monster{
+    private int numberOfAttacks;
+    private int damage;
 
-public class Mummy {
+    public Mummy(int life, int numberOfAttacks, int damage) {
+        super(life);
+        this.numberOfAttacks = 0;
+        this.damage = 40;
+    }
+
+    public Mummy(int life) {
+        super(life);
+    }
+
+    public int getNumberOfAttacks() {
+        return numberOfAttacks;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+    @Override
+    public void attack(int life, Monster monster2){
+        if (numberOfAttacks/3==0){
+            System.out.println("Sorry, but Mummy can't attack this time");
+            int newLife= life-10;
+        }else{
+            System.out.println("Mummy attacks!");
+            monster2.setLife(life-damage);
+        }
+    }
 }
