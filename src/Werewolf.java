1fc7edece456aaadda06d4b75c0959d39bce072f
csbsantos <claudiasbsantos13@gmail.com>
2025-09-26 17:08:37 +0100

    Update Werewolf.java

--- src/Werewolf.java
+++ src/Werewolf.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+public class Werewolf extends Monster {
+    private int damage=20;
 
-public class Werewolf {
+    public Werewolf(int life, int damage) {
+        super(life);
+        this.damage = damage;
+
+    }
+
+    public Werewolf(int life) {
+        super(life);
+    }
+
+    @Override
+    public void attack (int life, Monster monster2){
+        if (life>0){
+            System.out.println("Werewolf attacks");
+            monster2.setLife(monster2.getLife()-damage);
+        }
+    }
 }
